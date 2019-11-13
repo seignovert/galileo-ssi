0c305df86f29010f75d5563d36be1f735e8b8313
Benoit Seignovert <benoit.a.seignovert@jpl.nasa.gov>
2019-11-12 22:52:58 -0800

    Bump version to 1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='galileo-ssi',
-    version='1.2',
+    version='1.3',
     description='Python parser for Galileo SSI instrument',
     author='Benoit Seignovert',
     author_email='benoit.a.seignovert@jpl.nasa.gov',
